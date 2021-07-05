bb60dc48ab64adb63d63fc18e8a8d903b77b7eef
ni-xon <nixonuch_@hotmail.com>
2021-07-05 12:41:18 +1000

    Rearranged code and implemented mouse button down functionality

--- main.py
+++ main.py
@@ -2,21 +2,11 @@ import pygame
 from risk.constants import SQUARE_SIZE, WIDTH, HEIGHT
 from risk.board import Board
 
-FPS = 60
-WIN = pygame.display.set_mode((WIDTH, HEIGHT))
-pygame.display.set_caption('RISK')
-
 def get_row_col_from_mouse(pos):
     x_mouse, y_mouse = pos
     row = y_mouse // SQUARE_SIZE
     col = x_mouse // SQUARE_SIZE
     return row, col
-
-
-board = Board()
-board.create_initial_objects()
-piece = board.get_piece(1, 1) # For testing feel free to remove
-board.move(piece, 6, 6) # For testing feel free to remove
 
 def main():
     run = True
@@ -30,11 +20,21 @@ def main():
                 run = False
 
             if event.type == pygame.MOUSEBUTTONDOWN:
-                pass
-        
+                pos = pygame.mouse.get_pos()
+                row, col = get_row_col_from_mouse(pos)
+                piece = board.get_piece(row, col)
+                board.move(piece, 6, 6)
+
         # Draw the board and update pygame window screen
         board.draw(WIN)
         pygame.display.update()
 
     pygame.quit()
+
+FPS = 60
+WIN = pygame.display.set_mode((WIDTH, HEIGHT))
+pygame.display.set_caption('RISK')
+
+board = Board()
+board.create_initial_objects()
 main()
